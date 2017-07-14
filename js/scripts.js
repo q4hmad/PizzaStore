44cea9f4ac1598ca1639821f012dfed4b4cbbbf4
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-07-14 00:26:41 -0700

    final logic for constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,46 @@
-
 //Business logic
-// function Order(topping, size)  {
-//   this.topping = topping;
-//   this.size = size;
-// }
+function Order(topping, size) {
+  this.topping = topping;
+  this.size = size;
+}
 
-// Ticket.prototype.finalPrice = function() {
-//   if (this.topping === "Diehard1") {
-//     var price = "$1.00";
-//   } else if (this.age >= 65 || this.time === "1100") {
-//     var price = "$5.00";
-//   } else if (this.age < 65 && this.time !== "1100") {
-//     var price = "$8.00";
-//   }
-//   return (this.movie + ", playing at " + this.time + ", costs " + price);
-// }
+Order.prototype.pricing = function() {
+  if (this.size === 16 && this.topping <= 2) {
+    return ("The final price for your pizza is $16.00.")
+  } else if (this.size === 16 && this.topping >= 3) {
+    return ("the final price for your pizza is $18.00.")
+  } else if (this.size === 12 && this.topping <=2) {
+    return ("The final price for your pizza is $13.00.")
+  } else if (this.size === 12 && this.topping >= 3) {
+    return ("The final price of your pizza is $15.00.")
+  } else if (this.size === 8 && this.topping <= 2) {
+    return ("The final price of your pizza is $10.00.")
+  } else if (this.size === 8 && this.topping >= 3) {
+    return ("The final price of your pizza is $12.00.")
+  }
+
+
+}
+
 
 //Uswer interface logic
 $(document).ready(function() {
   $("form#topping_choices").submit(function(event) {
-  event.preventDefault();
-  // $("#finalPrice").show();
-  $("input:checkbox[name=topping-choices]:checked").each(function(){
-    var toppingsSelected = $(this).val();
-    console.log(toppingsSelected);
-  // $("#finalPrice").append(toppingsSelected + "br");
-});
-// $("#toppingChoices").hide();
-  var size = parseInt($("input:radio[name=size]:checked").val());
-  console.log(size);
-  // var yourPrice = new Order(toppings, size);
-  // console.log(yourPrice);
+    event.preventDefault();
+    var numberOfToppings = 0;
+
+    $("input:checkbox[name=topping-choices]:checked").each(function() {
+      numberOfToppings += 1;
+      var toppingsSelected = $(this).val();
+
+        console.log(numberOfToppings);
+      })
 
 
+
+      var size = parseInt($("input:radio[name=size]:checked").val());
+
+      var newOrder = new Order(numberOfToppings, size);
+      console.log(newOrder.pricing());
+    });
   });
-});
